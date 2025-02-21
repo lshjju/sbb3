8fdcb0b97c6e0ecb0eed2a30432112d42d8d8aea
lshjju <53454978+lshjju@users.noreply.github.com>
2025-02-21 13:34:02 +0900

    Add files via upload

---
+++ Do it! 점프 투 스프링 부트 3/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,33 @@
+package com.mysite.sbb;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+// 질문에 대한 답 제목과 내용을 담는 롤
+public class Answer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+	private LocalDateTime createDate;
+
+	// 매니투원 - 답은 많이 질문 하나
+	@ManyToOne
+	// 앤서의 퀘스천과 열결 롤
+	private Question question;
+
+}
