18e3dfdc542c6cf6e27a9d8fdc4d9a81d593dfe9
lshjju <53454978+lshjju@users.noreply.github.com>
2025-04-06 15:52:51 +0900

    Update AnswerForm.java

--- src/main/java/com/mysite/sbb/answer/AnswerForm.java
+++ src/main/java/com/mysite/sbb/answer/AnswerForm.java
@@ -7,11 +7,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-// 엔티티생성
 public class AnswerForm {
-	// QuestionForm과 같음
-	// 이것은 제목은 필요없음
-	// 내용만 있으면 됨
     @NotEmpty(message = "내용은 필수항목입니다.")
     private String content;
 }
